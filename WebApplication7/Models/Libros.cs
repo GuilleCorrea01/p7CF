edd744bace44544ee9b3e62bc95d596095786d37
Guillermo <guillecorrea2001@gmail.com>
2024-06-09 12:05:58 -0300

    primer push

---
+++ WebApplication7/Models/Libros.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication7.Models
+{
+    public class Libros
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Lib_Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Lib_Nombre { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Lib_Autor { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Lib_Genero { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Lib_TipoProyecto { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Lib_Status { get; set; }
+
+        [InverseProperty]
+        public ICollection<Personajes> Personajes { get; set; }
+    }
+}
